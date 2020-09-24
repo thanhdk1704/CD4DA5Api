369d8add32c6d5be17a6092b69a31625a869b262
dickdzaii <70245065+dickdzaii@users.noreply.github.com>
2020-09-24 15:33:01 +0700

    add interfaces

---
+++ DAL/Interfaces/IloaiCon2Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+namespace DAL.Interfaces
+{
+    public partial interface IloaiCon2Repository
+    {
+        bool Create(ItemModel model);
+    }
+}
